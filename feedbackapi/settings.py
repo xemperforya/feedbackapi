8dff00f8cd618df98d2379035404b61f103d544d
Shantanu <shantanu@simplifyvms.com>
2021-06-17 15:13:14 +0530

    updated readme

--- feedbackapi/settings.py
+++ feedbackapi/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testdb',
         'USER': 'root',
-        'PASSWORD': 'Shantanu@246',
+        'PASSWORD': '[password-here]',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
